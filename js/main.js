b5685cf4e7ab54dda3372d03dd242a6de5652254
Katarzyna K <k.kotynska91@gmail.com>
2016-04-25 13:05:19 +0200

    Fixed menu and z-index for photo in features section.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 $(function () {
     new WOW().init();
+    $(document).ready(function() {
+	    var NavY = $('.sec__box-sticky').offset().top;
+	     
+	    var stickyNav = function(){
+	    var ScrollY = $(window).scrollTop();
+	          
+	    if (ScrollY > NavY) { 
+	        $('.sec__box-sticky').addClass('sticky');
+	    } else {
+	        $('.sec__box-sticky').removeClass('sticky'); 
+	    }
+	    };
+	     
+	    stickyNav();
+	     
+	    $(window).scroll(function() {
+	        stickyNav();
+	    });
+    });
 });
+
+
+    
+    
