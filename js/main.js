5c4c00a36efe5e8f4cd5c2cc069ca76af6176fb8
Katarzyna K <k.kotynska91@gmail.com>
2016-04-27 16:59:31 +0200

    Update after Dominika's feedback.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 $(function () {
     new WOW().init();
     $(document).ready(function() {
-	    var NavY = $('.sec__box-sticky').offset().top;
+	    var NavY = $('.sec--head').offset().top;
 	     
 	    var stickyNav = function(){
 	    var ScrollY = $(window).scrollTop();
 	          
 	    if (ScrollY > NavY) { 
-	        $('.sec__box-sticky').addClass('sticky');
-	    } else {
-	        $('.sec__box-sticky').removeClass('sticky'); 
-	    }
+	        $('.sec--head').addClass('sticky');
+		    } else {
+		        $('.sec--head').removeClass('sticky'); 
+		    }
 	    };
 	     
 	    stickyNav();
